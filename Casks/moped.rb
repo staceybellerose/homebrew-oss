8b384559f21f336d20f34c1443c8b717e9be8271
Stacey Adams <stacey.adams@snapit.solutions>
2021-11-16 08:00:24 -0600

    Add moped cask

---
+++ Casks/moped.rb
@@ -0,0 +1,15 @@
+cask "moped" do
+  version "1.8.2"
+  sha256 "ea8e9c812baeb1168093479713caf80239da7725a77db55b7744fa35bb079978"
+
+  url "https://github.com/RobertoMachorro/Moped/releases/download/v#{version}/Moped-#{version}.zip",
+      verified: "github.com/RobertoMachorro/Moped/"
+  name "Moped"
+  desc "General purpose text editor"
+  homepage "https://roberto.machorro.net/Moped/"
+
+  app "Moped.app"
+
+  zap trash: "~/Library/Containers/net.machorro.roberto.Moped"
+done
+
