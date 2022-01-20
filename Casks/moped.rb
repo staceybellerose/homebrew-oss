aa23e64b314b6e3c152131c42731f6d38c2f5926
Stacey Adams <stacey.adams@snapit.solutions>
2022-01-20 09:45:20 -0600

    Update Moped cask to 1.8.3

--- Casks/moped.rb
+++ Casks/moped.rb
@@ -1,12 +1,14 @@
 cask "moped" do
-  version "1.8.2"
-  sha256 "ea8e9c812baeb1168093479713caf80239da7725a77db55b7744fa35bb079978"
+  version "1.8.3"
+  sha256 "974a78fa8c8d9a73d59fc6d6745f28fdd4a21c954bd47f8b4a68f1320464f382"
 
   url "https://github.com/RobertoMachorro/Moped/releases/download/v#{version}/Moped-#{version}.zip",
       verified: "github.com/RobertoMachorro/Moped/"
   name "Moped"
   desc "General purpose text editor"
   homepage "https://roberto.machorro.net/Moped/"
+
+  depends_on macos: ">= :big_sur"
 
   app "Moped.app"
 
